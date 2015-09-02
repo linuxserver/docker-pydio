e1a60b20e368809f773f385cbefa93ebbc003ad0
sparklyballs <sparklyballs@gmail.com>
2015-09-02 22:51:48 +0100

    place ssmtp config file

--- init/80_set_config.sh
+++ init/80_set_config.sh
@@ -20,3 +20,17 @@ cp /config/php/php-fpm.ini /etc/php5/fpm/php.ini
 
 chown -R abc:abc /config/log/pydio /config/php
 
+# setting email config file.
+
+if [ ! -f "/config/ssmtp.conf" ]; then
+
+cat <<'EOT' > /config/ssmtp.conf
+root=<your_email>
+mailhub=smtp.gmail.com:587
+AuthUser=<your_email>
+AuthPass=<your_password>
+UseSTARTTLS=YES
+EOT
+chown abc:abc /config/ssmtp.conf
+fi
+cp /config/ssmtp.conf /etc/ssmtp/ssmtp.conf
