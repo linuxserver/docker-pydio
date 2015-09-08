b0946ad7389e28d7dae057e1808e3bc586756c5f
sparklyballs <sparklyballs@gmail.com>
2015-09-08 14:03:22 +0100

    reverse committing earlier pydio version to test updating

--- init/70_get_the_app.sh
+++ init/70_get_the_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f "/config/www/pydio/index.php" ]; then
-wget -O /tmp/pydio.tar.gz http://sourceforge.net/projects/ajaxplorer/files/pydio/stable-channel/6.0.6/pydio-core-6.0.6.tar.gz
+wget -O /tmp/pydio.tar.gz http://sourceforge.net/projects/ajaxplorer/files/pydio/stable-channel/6.0.8/pydio-core-6.0.8.tar.gz
 cd /tmp
 tar -xvf pydio.tar.gz
 mv pydio-*/data/* /data/
